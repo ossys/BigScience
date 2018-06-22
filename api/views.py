560f6fc1f11206f6768de2e9348ec194006a2c58
ccravens <chad.cravens@ossys.com>
2018-06-22 15:43:16 -0400

    Added profile listing and creating feature

--- api/views.py
+++ api/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from . import serializers
-from django.template.context_processors import request
+from . import models
 
 # Create your views here.
 
@@ -104,3 +104,10 @@ class HelloViewSet(viewsets.ViewSet):
         
         return Response({'http_method': 'DELETE'})
 
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profiles."""
+    
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
